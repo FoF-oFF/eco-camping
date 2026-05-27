d3bf5415076028be8d2da2146aee17951f0e3092
FoF-oFF <jef.saavedra@duocuc.cl>
2026-05-26 21:23:43 -0400

    Add files via upload

--- ecocamping.py
+++ ecocamping.py
@@ -18,5 +18,23 @@ while ejecutando:
     if opcion == 1:
         disponibles = capacidad_maxima - sitios_ocupados
         print(f"\n[INFO] Sitios libres para recibir vehículos: {disponibles}")
+    #opcion 2
+    elif opcion == 2:
+        sitios_libres = capacidad_maxima - sitios_ocupados
+        if sitios_libres == 0:
+            print("Lo sentimos, no quedan espacios disponibles")
+        else:
+            try:
+                ingreso = int(input("¿Cuantos sitios o vehículos van a ingresar?"))
+                if ingreso <= 0:
+                    print("Error, la cantidad de ingreso sebe ser mayor a 0")
+                elif ingreso > sitios_libres:
+                    print(f"Solo puede ingresar un máximo de {sitios_libres} sitios")
+                else:
+                    sitios_ocupados += ingreso
+                    print(f"Ingreso registrado, se han ocupado {ingreso} de sitios")
+            except ValueError:
+                print("Error: debe ingresar un número válido")
     else:
         print("Opción fuera de rango")
+        
